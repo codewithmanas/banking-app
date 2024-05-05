262e916d1db42fd60c702ccf55f1692f07173273
Manas Ranjan Adha <manas865873@gmail.com>
2024-05-05 15:12:47 +0530

    implement HeaderBox and TotalBalanceBox

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import React from 'react';
 
 const Home = () => {
+
+    const loggedIn = { firstName: "Manas"};
+
   return (
-    <div>
-        <h1>This is Home Page</h1>
-    </div>
+    <section className='home'>
+        <div className='home-content'>
+          <div className="home-header">
+              <HeaderBox
+                type="greeting"
+                title="Welcome"
+                user={loggedIn?.firstName || "Guest"}
+                subtext="Access and manage your account and transaction efficiently."
+              />
+
+              <TotalBalanceBox
+                accounts={[]}
+                totalBanks={5}
+                totalCurrentBalance={1520.05}
+              />
+          </div>
+        </div>
+    </section>
   )
 }
 
